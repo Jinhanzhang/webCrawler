0713e4a97706a1536e995b309bc334eb056c6f59
Jinhan Zhang <jinhanzhangemit@gmail.com>
2021-11-28 11:40:58 -0500

    finished

---
+++ product_scraper/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Product(scrapy.Item):
+    name = scrapy.Field()
+    ingredients = scrapy.Field()
+    description = scrapy.Field()
+    img_url = scrapy.Field()
+    label = scrapy.Field()
+
+# class ProductScraperItem(scrapy.Item):
+#     # define the fields for your item here like:
+#     # name = scrapy.Field()
+#     pass
