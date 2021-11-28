3694a1e88d6b8aaaf0de4992af04a2ea1587a16d
Jinhan Zhang <jinhanzhangemit@gmail.com>
2021-11-28 11:40:58 -0500

    finished

---
+++ product_scraper/spiders/sitemap_spider.py
@@ -0,0 +1,32 @@
+import scrapy
+
+from product_scraper.items import Product
+from scrapy.spiders import SitemapSpider, Rule
+
+class SitemapSpider(SitemapSpider):
+    name = "sitemap_spider"
+    sitemap_urls = ['https://www.allrecipes.com/sitemap.xml']
+    sitemap_rules = [
+        ('/recipe/', 'parse_product')
+    ]
+
+    def parse_product(self, response):
+        item = Product()
+        item['name'] = response.xpath("//h1[contains(@class,'headline') and contains(@class,'heading-content')]/text()").get()
+        ingredients = response.xpath("//input[@data-tracking-label='ingredient clicked']")
+        ingredients_obj = []
+        for ingredient in ingredients:
+            ingredient_obj = {
+                'name': ingredient.xpath(".//@data-ingredient").get(),
+                'qty': ingredient.xpath(".//@data-quantity").get(),
+                'unit': ingredient.xpath(".//@data-unit").get(),
+            }
+            ingredients_obj.append(ingredient_obj)
+        item['ingredients'] = ingredients_obj
+        item['description'] = response.xpath("//li[contains(@class,'instructions-section-item')]//div//p/text()").getall()
+        item['img_url'] = response.xpath("//div[contains(@class,'primary-media-with-filmstrip')]//div//noscript//img/@src").get(0)
+        item['label'] = response.xpath("//li[@class='breadcrumbs__item'][3]//a/span/text()").get()
+        # item['price'] = response.xpath("//div[@class='my-4']/span/text()").get()
+        # item['title'] = response.xpath('//section[1]//h2/text()').get()
+        # item['img_url'] = response.xpath("//div[@class='product-slider']//img/@src").get(0)
+        return item
